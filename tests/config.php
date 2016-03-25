4837db5c2acf2cf51d14ee4bb7b46ba35f3a92b2
zhengluming <908235332@qq.com>
2016-03-25 09:10:37 +0800

    测试数据库设计

--- tests/config.php
+++ tests/config.php
@@ -9,30 +9,30 @@ $configs = array(
         'db1' => array(
             'class' => 'LumengPHP\Db\Connection\SimpleConnection',
             //表前缀，如：bbs_
-            'tablePrefix' => '',
+            'tablePrefix' => 'bbs_',
             //数据库字符集
             'charset' => 'utf8',
             //数据库配置
-            'dsn' => 'mysql:host=127.0.0.1;dbname=bbs',
-            'username' => '',
-            'password' => '',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=bbsdb',
+            'username' => 'bbs',
+            'password' => 'bbs',
         ),
         //second connection
         'db2' => array(
             'class' => 'LumengPHP\Db\Connection\MasterSlaveConnection',
             //表前缀，如：bbs_
-            'tablePrefix' => '',
+            'tablePrefix' => 'bbs_',
             //数据库字符集
             'charset' => 'utf8',
             //数据库服务器列表，第一个为master服务器，剩下的为从服务器
             'servers' => array(
                 array(
-                    'dsn' => 'mysql:host=127.0.0.1;dbname=bbs',
+                    'dsn' => 'mysql:host=dbhost1;dbname=bbs',
                     'username' => '',
                     'password' => '',
                 ),
                 array(
-                    'dsn' => 'mysql:host=127.0.0.1;dbname=bbs',
+                    'dsn' => 'mysql:host=dbhost2;dbname=bbs',
                     'username' => '',
                     'password' => '',
                 ),
--- tests/resources/database.sql
+++ tests/resources/database.sql
@@ -1,7 +1,9 @@
 
-/*
- * 测试用的数据库
- */
+-- 测试用的数据库
+-- 数据库名称：bbsdb
+-- 数据库用户名：bbs
+-- 数据库密码：bbs
+-- 表前缀：bbs_
 
 CREATE TABLE `bbs_user` (
  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -17,15 +19,11 @@ CREATE TABLE `bbs_user` (
  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-CREATE TABLE `bbs_user_profile` (
- `uid` int(10) unsigned NOT NULL COMMENT '用户id，主键',
- `sex` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '性别：1,男；0,女。默认为1',
- `location` varchar(30) NOT NULL COMMENT '居住地',
- `business` varchar(30) NOT NULL COMMENT '所属行业',
- `employment` varchar(80) NOT NULL COMMENT '所属公司或组织名称',
- `position` varchar(40) NOT NULL COMMENT '职位',
- `education` varchar(70) NOT NULL COMMENT '（毕业）学校或教育机构',
- `speciality` varchar(35) NOT NULL COMMENT '专业方向，如“软件工程”',
- `description` varchar(600) NOT NULL COMMENT '个人简介',
- PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户资料表';
+CREATE TABLE `bbs_post` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `uid` int(10) unsigned NOT NULL COMMENT '所属用户id',
+ `title` varchar(40) NOT NULL COMMENT '帖子标题',
+ `content` text NOT NULL COMMENT '帖子内容',
+ `add_time` int(10) unsigned NOT NULL COMMENT '帖子创建时间',
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='(用户)发帖表';
