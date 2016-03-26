a764325113e1c3a4be362c6d1a8caaaba2d2b05a
zhengluming <908235332@qq.com>
2016-03-26 23:19:32 +0800

    插入数据测试用例编写

---
+++ tests/TestCases/InsertTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tests\TestCases;
+
+use tests\Model\UserModel;
+
+/**
+ * 数据插入测试
+ *
+ * @author Lumeng <zhengb302@163.com>
+ */
+class InsertTest extends BaseDatabaseTestCase {
+
+    protected function getDataSet() {
+        $xmlFileArray = array(
+            TEST_ROOT . '/resources/insert-fixture.xml',
+        );
+        return $this->createCompositeMySQLXMLDataSet($xmlFileArray);
+    }
+
+    public function testInsert() {
+        $userModel = new UserModel();
+
+        $data = array(
+            'username' => 'zhangsan',
+            'password' => md5('123456'),
+            'email' => 'zhangsan@126.com',
+            'nickname' => '张三',
+            'add_time' => '1409305283',
+        );
+
+        $newUid = $userModel->add($data);
+
+        $this->assertEquals(4, $newUid);
+
+        $newUser = $userModel->where(array('uid' => $newUid))->find();
+
+        $this->assertEquals('张三', $newUser['nickname']);
+    }
+
+}
