1433136f18286520573c1771737a25771e3583f9
zhengluming <908235332@qq.com>
2016-03-27 20:42:56 +0800

    数据库事务测试用例编写

---
+++ tests/TestCases/TransactionTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace tests\TestCases;
+
+use tests\Model\UserModel;
+use tests\Model\PostModel;
+
+/**
+ * 事务测试
+ *
+ * @author Lumeng <zhengb302@163.com>
+ */
+class TransactionTest extends BaseDatabaseTestCase {
+
+    protected function getDataSet() {
+        $xmlFileArray = array(
+            TEST_ROOT . '/resources/transaction-fixture.xml',
+        );
+        return $this->createCompositeMySQLXMLDataSet($xmlFileArray);
+    }
+
+    public function testCommit() {
+        $userModel = new UserModel();
+        $postModel = new PostModel();
+
+        global $connManager;
+        $conn = $connManager->getConnection();
+
+        $conn->beginTransaction();
+
+        //删除李雷
+        $userModel->where(array('username' => 'lilei'))->delete();
+
+        //删除李雷的发帖
+        $postModel->where(array('uid' => 2))->delete();
+
+        $conn->commit();
+
+        //现在只剩下三个用户
+        $this->assertEquals(3, $userModel->count());
+        //李雷已被删除，再查找这个用户，会返回null
+        $lilei = $userModel->where(array('username' => 'lilei'))->find();
+        $this->assertNull($lilei);
+        //查找李雷的发帖，也返回null
+        $postsOfLilei = $postModel->where(array('uid' => 2))->select();
+        $this->assertNull($postsOfLilei);
+    }
+
+    public function testRollback() {
+        $userModel = new UserModel();
+        $postModel = new PostModel();
+
+        global $connManager;
+        $conn = $connManager->getConnection();
+
+        $conn->beginTransaction();
+
+        //删除李雷
+        $userModel->where(array('username' => 'lilei'))->delete();
+
+        //删除李雷的发帖
+        $postModel->where(array('uid' => 2))->delete();
+
+        $conn->rollback();
+
+        //因为回滚了事务，现在还是四个用户
+        $this->assertEquals(4, $userModel->count());
+        //因为回滚了事务，再查找李雷，李雷的用户数据并没有被删除
+        $lilei = $userModel->where(array('username' => 'lilei'))->find();
+        $this->assertNotNull($lilei);
+        $this->assertEquals('李雷', $lilei['nickname']);
+        //因为回滚了事务，查找李雷的发帖，都还在
+        $postsOfLilei = $postModel->where(array('uid' => 2))->select();
+        $this->assertNotNull($postsOfLilei);
+        $this->assertCount(3, $postsOfLilei);
+    }
+
+}
