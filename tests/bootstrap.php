b12cbb4c1d886c63476f7cac665c6fcec8a9d681
zhengluming <908235332@qq.com>
2017-06-07 10:48:28 +0800

    删除没用的类

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,7 @@ define('TEST_ROOT', __DIR__);
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 $loader->add('tests\\', dirname(__DIR__));
 
-use LumengPHP\Db\Misc\ShortcutFunctionHelper;
 use LumengPHP\Db\ConnectionManager;
-
-//load shortcut functions
-require(ShortcutFunctionHelper::getPath());
 
 $configs = require(__DIR__ . '/config.php');
 $connectionConfigs = $configs['connections'];
