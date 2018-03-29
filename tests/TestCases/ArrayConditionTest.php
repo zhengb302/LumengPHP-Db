79b981345f38a218267e6ebca8d486ae65301e2e
zhengluming <908235332@qq.com>
2018-03-29 11:46:08 +0800

    更新测试用例注释

--- tests/TestCases/ArrayConditionTest.php
+++ tests/TestCases/ArrayConditionTest.php
@@ -17,6 +17,8 @@ class ArrayConditionTest extends \PHPUnit_Framework_TestCase {
      * @expectedExceptionMessage 无效的条件值类型，字段名称：username，值类型：NULL
      */
     public function testNullValue() {
+        //条件值为null，很大的可能是未检查过滤用户的输入，直接使用原始的输入值来做查询条件
+        //框架采取的做法是抛出异常，提醒开发者要对用户输入做检查
         $conditions = [
             'username' => null,
         ];
